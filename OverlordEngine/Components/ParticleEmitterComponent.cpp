55be54295eb32f9b6d159151e0c6eb5aa9521639
TheTwitchDude81 <51368164+MaximRobeyst@users.noreply.github.com>
2022-04-20 08:15:30 +0200

    Start Particle Component

--- OverlordEngine/Components/ParticleEmitterComponent.cpp
+++ OverlordEngine/Components/ParticleEmitterComponent.cpp
@@ -16,22 +16,49 @@ ParticleEmitterComponent::ParticleEmitterComponent(const std::wstring& assetFile
 
 ParticleEmitterComponent::~ParticleEmitterComponent()
 {
-	TODO_W9(L"Implement Destructor")
+	delete m_ParticlesArray;
+	m_pVertexBuffer->Release();
 }
 
-void ParticleEmitterComponent::Initialize(const SceneContext& /*sceneContext*/)
+void ParticleEmitterComponent::Initialize(const SceneContext& sceneContext)
 {
-	TODO_W9(L"Implement Initialize")
+	m_pParticleMaterial = MaterialManager::Get()->GetMaterial<ParticleMaterial>(0);
+	CreateVertexBuffer(sceneContext);
+	m_pParticleTexture = ContentManager::Load<TextureData>(m_AssetFile);
 }
 
-void ParticleEmitterComponent::CreateVertexBuffer(const SceneContext& /*sceneContext*/)
+void ParticleEmitterComponent::CreateVertexBuffer(const SceneContext& sceneContext)
 {
-	TODO_W9(L"Implement CreateVertexBuffer")
+	if (m_pVertexBuffer)
+		m_pVertexBuffer->Release();
+
+	D3D11_BUFFER_DESC vertexBufferDesc{};
+	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	vertexBufferDesc.ByteWidth = sizeof(VertexParticle) * m_ParticleCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	vertexBufferDesc.MiscFlags = 0;
+	HANDLE_ERROR(sceneContext.d3dContext.pDevice->CreateBuffer(&vertexBufferDesc, nullptr, &m_pVertexBuffer))
+
 }
 
-void ParticleEmitterComponent::Update(const SceneContext& /*sceneContext*/)
+void ParticleEmitterComponent::Update(const SceneContext& sceneContext)
 {
-	TODO_W9(L"Implement Update")
+	float particleInterval = ((m_EmitterSettings.maxEnergy + m_EmitterSettings.minEnergy) / 2) / m_MaxParticles;
+	m_LastParticleSpawn += sceneContext.pGameTime->GetElapsed();
+	m_ActiveParticles = 0;
+	VertexParticle* pBuffer{ nullptr };
+	D3D11_MAPPED_SUBRESOURCE* pData = reinterpret_cast<D3D11_MAPPED_SUBRESOURCE*>(pBuffer);
+	sceneContext.d3dContext.pDeviceContext->Map(m_pVertexBuffer, 1, D3D11_MAP_WRITE_DISCARD, 0, pData);
+	for (auto i = 0; i < m_ParticleCount; ++i)
+	{
+		auto currentParticle = m_ParticlesArray[i];
+
+		if (currentParticle.isActive)
+			UpdateParticle(currentParticle, sceneContext.pGameTime->GetElapsed());
+		if (!currentParticle.isActive && m_LastParticleSpawn >= particleInterval)
+			SpawnParticle(currentParticle);
+	}
 }
 
 void ParticleEmitterComponent::UpdateParticle(Particle& /*p*/, float /*elapsedTime*/) const
